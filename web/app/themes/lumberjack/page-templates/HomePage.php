44112633f78356f71c007c69c6ae340ffc6f8ca6
IHIutch <jbhutch01@gmail.com>
2020-03-30 21:24:44 -0400

    Add homepage template and acf fields

---
+++ web/app/themes/lumberjack/page-templates/HomePage.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+Template Name: Home Page 
+*/
+
+namespace App;
+
+use App\Http\Controllers\Controller;
+use Rareloop\Lumberjack\Http\Responses\TimberResponse;
+use Rareloop\Lumberjack\Page;
+use Timber\Timber;
+
+class PageController extends Controller
+{
+    public function handle()
+    {
+        $context = Timber::get_context();
+        $page = new Page();
+
+        $context['post'] = $page;
+        $context['title'] = $page->title;
+        $context['content'] = $page->content;
+
+        return new TimberResponse('templates/generic-page.twig', $context);
+    }
+}
