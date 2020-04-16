5bae6be3852ba6882e090f643ee424c7ec6c67a4
IHIutch <jbhutch01@gmail.com>
2020-04-15 20:11:55 -0400

    enqueue assets consistency

--- web/app/themes/lumberjack/app/Functions/Assets.php
+++ web/app/themes/lumberjack/app/Functions/Assets.php
@@ -16,8 +16,8 @@ class Assets
             wp_register_style('main-css', get_template_directory_uri() . '/dist/css/style.min.css', $style_path, null);
             wp_enqueue_style('main-css');
             // scripts
-            $js_path = get_template_directory() . '/dist/js/script.min.js';
-            wp_register_script('main-scripts', get_template_directory_uri() . '/dist/js/script.min.js', ['jquery'], filemtime($js_path), true);
+            $js_path = filemtime(get_template_directory() . '/dist/js/script.min.js');
+            wp_register_script('main-scripts', get_template_directory_uri() . '/dist/js/script.min.js', ['jquery'], $js_path, true);
             wp_enqueue_script('main-scripts');
         });
     }
