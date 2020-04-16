dc14f1428e85c95345ba5f07d69a2830e6a971cd
IHIutch <jbhutch01@gmail.com>
2020-04-15 20:25:45 -0400

    src -> assets

--- web/app/themes/lumberjack/webpack.config.js
+++ web/app/themes/lumberjack/webpack.config.js
@@ -10,43 +10,47 @@ const config = {
   //Your local development url for browsersync
   localUrl: "http://abacapress:8888/",
   //Open new window each time 'npm run serve' command is executed
-  openWindow: false
+  openWindow: false,
 };
 
 module.exports = {
   optimization: {
     minimizer: [
       new TerserJSPlugin({
-        sourceMap: true
+        sourceMap: true,
       }),
       new OptimizeCSSAssetsPlugin({
         cssProcessorOptions: {
           map: {
-            inline: false
-          }
-        }
-      })
-    ]
+            inline: false,
+          },
+        },
+      }),
+    ],
   },
   devtool: "source-map",
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "css/style.min.css"
+      filename: "css/style.min.css",
     }),
     new BrowserSyncPlugin({
       proxy: config.localUrl,
       files: ["**/*.php", "**/*.twig"],
       reloadDelay: 0,
-      open: config.openWindow
-    })
+      open: config.openWindow,
+    }),
   ],
   entry: {
-    assets: ["@babel/polyfill", "./src/js/app.js", "./src/scss/main.scss"]
+    assets: [
+      "@babel/polyfill",
+      "./assets/js/app.js",
+      "./assets/scss/main.scss",
+    ],
   },
   output: {
     path: path.resolve(__dirname, "./dist/"),
     filename: "js/script.min.js",
-    publicPath: config.distPath
+    publicPath: config.distPath,
   },
   module: {
     rules: [
@@ -60,12 +64,12 @@ module.exports = {
               "@babel/preset-env",
               {
                 useBuiltIns: "entry",
-                corejs: 3
-              }
-            ]
+                corejs: 3,
+              },
+            ],
           ],
-          cwd: "./"
-        }
+          cwd: "./",
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -73,13 +77,13 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           "css-loader",
           "postcss-loader",
-          "sass-loader"
-        ]
+          "sass-loader",
+        ],
       },
       {
         test: /\.(jpg|svg|woff|woff2|ttf|eot|png|jpeg)(\?.*$|$)/,
-        loader: "file-loader"
-      }
-    ]
-  }
+        loader: "file-loader",
+      },
+    ],
+  },
 };
