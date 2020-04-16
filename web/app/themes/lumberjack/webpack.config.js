d2053843fee673c8e768cfe9ec1912e0dee20096
IHIutch <jbhutch01@gmail.com>
2020-04-16 00:29:26 -0400

    Make purgecss work

--- web/app/themes/lumberjack/webpack.config.js
+++ web/app/themes/lumberjack/webpack.config.js
@@ -4,11 +4,6 @@ const path = require("path");
 const TerserJSPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./**/*.twig"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 const config = {
   //Your path to dist accessed from a browser
   distPath: __dirname + "/dist/",
