01e917a733e56f59a867bc86d1812cd0a66deb5e
IHIutch <jbhutch01@gmail.com>
2020-04-19 21:52:00 -0400

    Build out contact

--- web/app/themes/lumberjack/tailwind.config.js
+++ web/app/themes/lumberjack/tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   theme: {
-    colors: {
-      tan: "#F7F0E9",
-      brown: "#3D1F00",
-      red: "#FF4552",
+    extend: {
+      spacing: {
+        "2/3": "66.666667%",
+        "14": "3.5rem",
+      },
+      colors: {
+        tan: "#F7F0E9",
+        brown: "#3D1F00",
+        red: "#FF4552",
+      },
     },
   },
 };
