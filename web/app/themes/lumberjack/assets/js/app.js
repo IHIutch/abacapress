3a54a66fae837c9b2807ab7c7a63b86dc4b97b8b
IHIutch <jbhutch01@gmail.com>
2020-04-27 20:55:03 -0400

    Break up some javascript

--- web/app/themes/lumberjack/assets/js/app.js
+++ web/app/themes/lumberjack/assets/js/app.js
@@ -1,22 +1,7 @@
+import Swiper from "swiper";
+import feather from "feather-icons";
 import "typeface-barlow-condensed";
 import "typeface-karla";
 
-import feather from "feather-icons";
-feather.replace();
-
-import Swiper from "swiper";
-var mySwiper = new Swiper(".swiper-container", {
-  slidesPerView: 3,
-  loop: true,
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
+window.Swiper = Swiper;
+window.feather = feather;
