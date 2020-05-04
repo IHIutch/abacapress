6cd502e6a1ca81af6874ca91a315b9e5e8dd96c8
IHIutch <jbhutch01@gmail.com>
2020-05-03 20:47:52 -0400

    Dont need to add alpine to window

--- web/app/themes/lumberjack/assets/js/app.js
+++ web/app/themes/lumberjack/assets/js/app.js
@@ -1,9 +1,8 @@
 import Swiper from "swiper";
 import feather from "feather-icons";
-import alpine from "alpinejs";
+import "alpinejs";
 import "typeface-barlow-condensed";
 import "typeface-karla";
 
 window.Swiper = Swiper;
 window.feather = feather;
-window.alpine = alpine;
