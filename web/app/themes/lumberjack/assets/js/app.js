8c76ab7e18e533ee32917685f4067ef478d1a13a
IHIutch <jbhutch01@gmail.com>
2020-05-03 20:46:19 -0400

    Install alpinejs

--- web/app/themes/lumberjack/assets/js/app.js
+++ web/app/themes/lumberjack/assets/js/app.js
@@ -1,7 +1,9 @@
 import Swiper from "swiper";
 import feather from "feather-icons";
+import alpine from "alpinejs";
 import "typeface-barlow-condensed";
 import "typeface-karla";
 
 window.Swiper = Swiper;
 window.feather = feather;
+window.alpine = alpine;
