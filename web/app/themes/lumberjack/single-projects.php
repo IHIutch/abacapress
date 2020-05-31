272d95688cae3faf82ca2aec338ddaa33612b8c5
IHIutch <jbhutch01@gmail.com>
2020-05-31 10:00:16 -0400

    Show additional posts on bottom of posts pages

--- web/app/themes/lumberjack/single-projects.php
+++ web/app/themes/lumberjack/single-projects.php
@@ -1,24 +1,25 @@
 <?php
 
 /**
- * The Template for displaying all single posts
+ * The Template for displaying all single project posts
  */
 
 namespace App;
 
 use App\Http\Controllers\Controller;
+use App\PostTypes\Project;
 use Rareloop\Lumberjack\Http\Responses\TimberResponse;
 use Rareloop\Lumberjack\Post;
 use Timber\Timber;
 
-class SingleController extends Controller
+class SingleProjectsController extends Controller
 {
     public function handle()
     {
         $context = Timber::get_context();
         $post = new Post();
 
-        $more_posts = Post::builder()
+        $more_posts = Project::builder()
             ->orderBy('date', 'asc')
             ->limit(6)
             ->get();
