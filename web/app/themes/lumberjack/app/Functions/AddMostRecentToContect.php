1ebd895fcbeb5d87dc1a517bba6526b120388c81
IHIutch <jbhutch01@gmail.com>
2020-05-30 22:06:32 -0400

    Force blog link to most recent post, dont need redirect

---
+++ web/app/themes/lumberjack/app/Functions/AddMostRecentToContect.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Functions;
+
+use Rareloop\Lumberjack\Post;
+
+class AddMostRecentToContext
+{
+    public static function mostRecentBlogToContext()
+    {
+        add_filter('timber_context', function ($context) {
+            $posts = Post::builder()
+                ->orderBy('date', 'asc')
+                ->limit(1)
+                ->get();
+
+            $context['most_recent_blog'] = $posts[0];
+
+            return $context;
+        });
+    }
+}
--- web/app/themes/lumberjack/routes.php
+++ web/app/themes/lumberjack/routes.php
@@ -1,14 +1,14 @@
 <?php
 
-use Rareloop\Lumberjack\Facades\Router;
-use Zend\Diactoros\Response\RedirectResponse;
-use Rareloop\Lumberjack\Post;
+// use Rareloop\Lumberjack\Facades\Router;
+// use Zend\Diactoros\Response\RedirectResponse;
+// use Rareloop\Lumberjack\Post;
 
-Router::get('/most-recent', function () {
-    $posts = Post::builder()
-        ->orderBy('date', 'asc')
-        ->limit(1)
-        ->get();
+// Router::get('/most-recent', function () {
+//     $posts = Post::builder()
+//         ->orderBy('date', 'asc')
+//         ->limit(1)
+//         ->get();
 
-    return new RedirectResponse($posts[0]->link);
-});
+//     return new RedirectResponse($posts[0]->link);
+// });
